1a83f5beb6cdccea2c93328bd58c4dc1fb135a99
sj1236517 <sj1236517@gmail.com>
2021-09-13 12:06:03 +0530

    first commit

---
+++ readme.md
@@ -0,0 +1,15 @@
+#Description
+##This is an instagram automation project
+####This project takes username and password and open your firefox browser and login in your instagram account and take screenshot of your news feed
+###for run this project you have to do run this command
+```bash
+pip install requirment.txt
+```
+##Download firefox link "https://github.com/mozilla/geckodriver/releases"
+After downloading the firefox you have to give the path where the firefox is installed
+ After giving path just run
+```bash
+python scraper.py
+```
+and enter your instagram username and password
+##Now you all set to go
---
+++ scraper.py
@@ -0,0 +1,25 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+
+def scraper(username, password):
+    driver = webdriver.Firefox(executable_path='/home/sanjay/Desktop/selenium_project/geckodriver')
+    # give here firefox path where it is installed
+    driver.get("https://www.instagram.com")
+    driver.implicitly_wait(30)
+    driver.find_element_by_name("username").send_keys(username)
+    driver.implicitly_wait(30)
+    driver.find_element_by_name("password").send_keys(password)
+    driver.implicitly_wait(30)
+    driver.find_element_by_name('password').send_keys(Keys.ENTER)
+    driver.implicitly_wait(30)
+    driver.find_element_by_xpath("/html/body/div[1]/section/main/div/div/div/div/button").click()
+    driver.implicitly_wait(30)
+    driver.find_element_by_xpath("/html/body/div[5]/div/div/div/div[3]/button[2]").click()
+    driver.save_screenshot("screenshot.png")
+
+
+if __name__ == "__main__":
+    username = input("Enter your instagram username")
+    password = input("Enter your instagram password")
+    scraper(username, password)
